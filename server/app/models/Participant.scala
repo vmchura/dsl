ab83ebebc0d06e2b15a0c95dcbf620c48caef260
vmchura <vmchura@gmail.com>
2020-07-07 22:44:20 +0000

    removed unused import

--- server/app/models/Participant.scala
+++ server/app/models/Participant.scala
@@ -4,15 +4,13 @@ import java.util.UUID
 import play.api.libs.json._
 
 
-case class ParticipantPK(tournamentID: UUID,chanameID: String)
+case class ParticipantPK(tournamentID: UUID,chaNameID: String)
 object ParticipantPK{
   implicit val jsonFormat: OFormat[ParticipantPK] = Json.format[ParticipantPK]
 }
 
 case class Participant(participantPK: ParticipantPK, discordUserID: Option[String], userID: Option[UUID])
 object Participant {
-  import ParticipantPK.jsonFormat
   implicit val jsonFormat: OFormat[Participant] = Json.format[Participant]
-
 }
 
