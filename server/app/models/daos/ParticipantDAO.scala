b3f64524705d14abdb64cbefea789c1c7a78ffa1
vmchura <vmchura@gmail.com>
2020-07-08 15:01:25 +0000

    Participants by tournament

--- server/app/models/daos/ParticipantDAO.scala
+++ server/app/models/daos/ParticipantDAO.scala
@@ -1,5 +1,7 @@
 package models.daos
 
+import java.util.UUID
+
 import models.{Participant, ParticipantPK}
 
 import scala.concurrent.Future
@@ -7,4 +9,5 @@ import scala.concurrent.Future
 trait ParticipantDAO {
   def find(participantPK: ParticipantPK): Future[Option[Participant]]
   def save(participant: Participant): Future[Boolean]
+  def find(tournamentID: UUID): Future[Seq[Participant]]
 }
--- server/app/models/daos/ParticipantDAOImpl.scala
+++ server/app/models/daos/ParticipantDAOImpl.scala
@@ -1,9 +1,12 @@
 package models.daos
 
+import java.util.UUID
+
 import javax.inject.Inject
 import models.{Participant, ParticipantPK}
 import play.api.libs.json.Json
 import play.modules.reactivemongo.ReactiveMongoApi
+import reactivemongo.api.Cursor
 import reactivemongo.play.json.collection.JSONCollection
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -23,5 +26,10 @@ class ParticipantDAOImpl  @Inject() (val reactiveMongoApi: ReactiveMongoApi) ext
       one(Json.obj("participantPK" -> participant.participantPK), participant, upsert = true)).
       map(_.ok)
 
+  }
+
+  override def find(tournamentID: UUID): Future[Seq[Participant]] = {
+    val query = Json.obj("participantPK.tournamentID" -> tournamentID)
+    collection.flatMap(_.find(query,Option.empty[Participant]).cursor[Participant]().collect[List](-1,Cursor.FailOnError[List[Participant]]()))
   }
 }
