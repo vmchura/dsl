9392c0bf78135c44e11ae2871c9946b246fba979
vmchura <vmchura@gmail.com>
2020-07-18 16:30:16 +0000

    TournamentController Impl

--- server/app/controllers/TournamentController.scala
+++ server/app/controllers/TournamentController.scala
@@ -14,6 +14,7 @@ import upickle.default._
 class TournamentController @Inject()(scc: SilhouetteControllerComponents,
                                      createTournamentView: views.html.createtournament,
                                      matchpairs: views.html.matchpairs,
+                                     showmatches: views.html.matches,
                                      tournamentBuilder: TournamentBuilder
                            ) (
                              implicit
@@ -30,20 +31,39 @@ class TournamentController @Inject()(scc: SilhouetteControllerComponents,
       data => {
         tournamentBuilder.buildTournament(data.discordGuildID,data.challongeID).map{
           case Left(error: CannontAccessChallongeTournament) =>
+            logger.error(error.toString)
             BadRequest(createTournamentView(CreateTournamentForm.form.
               fill(CreateTournamentForm.Data(data.discordGuildID,""))))
           case Left(error: CannotAccesDiscordGuild) =>
+            logger.error(error.toString)
             BadRequest(createTournamentView(CreateTournamentForm.form.
               fill(CreateTournamentForm.Data("",data.challongeID))))
           case Left(error) =>
+            logger.error(error.toString)
             BadRequest(createTournamentView(CreateTournamentForm.form))
-          case Right((tournament,participants, discordusers)) => Ok(matchpairs(tournament,
-            participants.map(p => BasicComparableByLabel(p.chaname,write(p.participantPK))),
-            discordusers.map(p => BasicComparableByLabel(p.userName, write(p.discordID)))))
+          case Right(tournament) => Redirect(routes.TournamentController.showparticipantscorrelation(tournament.challongeID))
+
         }
 
       }
     )
+  }
+
+  def showparticipantscorrelation(tournamentID: Long): Action[AnyContent] = silhouette.SecuredAction.async{ implicit request: SecuredRequest[EnvType, AnyContent] =>
+    tournamentBuilder.getParticipantsUsers(tournamentID).map{
+      case Left(error) =>Ok(s"error: ${error.toString}")
+      case Right((tournament,participants, discordusers)) => Ok(matchpairs(tournament,
+        participants.map(p => BasicComparableByLabel(p.chaname,write(p.participantPK))),
+        discordusers.map(p => BasicComparableByLabel(p.userName, write(p.discordID)))))
+    }
+
+  }
+  def showMatches(challongeTournamentID: Long): Action[AnyContent] = silhouette.SecuredAction.async { implicit request: SecuredRequest[EnvType, AnyContent] =>
+
+    tournamentBuilder.getMatches(challongeTournamentID,Some(request.identity)).map{
+      case Left(error) =>Ok(s"error: ${error.toString}")
+      case Right(matches) => Ok(showmatches(matches))
+    }
   }
 
 
