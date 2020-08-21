3eaebcfa1061a866a0bd768427ca70c80ed6521f
vmchura <vmchura@gmail.com>
2020-08-20 22:38:43 -0500

    ParseReplayFileService Module

--- server/app/models/services/ParseReplayFileService.scala
+++ server/app/models/services/ParseReplayFileService.scala
@@ -2,12 +2,36 @@ package models.services
 
 import java.io.File
 
+import play.api.libs.json.{JsArray, Json}
 import shared.models.{ActionByReplay, ReplayDescriptionShared}
 
 import scala.concurrent.Future
 
 trait ParseReplayFileService {
   def parseFile(file: File): Future[Either[String,String]]
-  def parseJsonResponse(stringJson: String): Either[String,ReplayDescriptionShared]
+  def parseJsonResponse(stringJson: String): Either[String,ReplayDescriptionShared] = {
+
+    val json = Json.parse(stringJson)
+    val playersJson = (json \ "Header" \ "Players").getOrElse(JsArray.empty).asInstanceOf[JsArray]
+    case class Player(team: Int, name: String)
+    val players = playersJson.value.toList.flatMap{ p =>
+      for{
+        team <- (p \ "Team").asOpt[Int]
+        name <- (p \ "Name").asOpt[String]
+      }yield{
+        Player(team,name)
+      }
+
+    }
+
+    (for{
+      p1 <- players.find(_.team ==1).map(_.name)
+      p2 <- players.find(_.team ==2).map(_.name)
+      winnerTeam <- (json \ "Computed" \ "WinnerTeam").asOpt[Int]
+    }yield{
+      Right(ReplayDescriptionShared(p1,p2, winnerTeam))
+    }).getOrElse(Left("Cant find players"))
+
+  }
   def parseFileAndBuildAction(file: File, discordUserID1: String, discordUserID2: String): Future[Either[String,ActionByReplay]]
 }
---
+++ server/app/models/services/ParseReplayFileServiceEmptyImpl.scala
@@ -0,0 +1,14 @@
+package models.services
+
+import java.io.File
+
+import shared.models.ActionBySmurf.{ActionBySmurf, SmurfsEmpty}
+import shared.models.{ActionByReplay, ReplayDescriptionShared}
+
+import scala.concurrent.Future
+
+class ParseReplayFileServiceEmptyImpl extends ParseReplayFileService {
+  def parseFile(file: File): Future[Either[String,String]] = Future.successful(Left("NOT IMPLEMENTED FUNCTION"))
+  def parseFileAndBuildAction(file: File, discordUserID1: String, discordUserID2: String): Future[Either[String,ActionByReplay]] =
+    Future.successful(Right(ActionByReplay(defined = true,Some("Flash"),Some("Bisu"),SmurfsEmpty,1)))
+}
