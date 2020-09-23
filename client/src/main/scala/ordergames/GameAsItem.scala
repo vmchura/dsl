391920bf8dd111e6bacb042758265f52c20f1d57
vmchura <vmchura@gmail.com>
2020-09-23 17:50:12 -0500

    CSS Ordering

--- client/src/main/scala/ordergames/GameAsItem.scala
+++ client/src/main/scala/ordergames/GameAsItem.scala
@@ -5,12 +5,14 @@ import org.lrng.binding.html.NodeBinding
 import org.scalajs.dom.raw.HTMLInputElement
 import shared.models.ReplayRecordShared
 
-case class GameAsItem(replayRecordShared: ReplayRecordShared, order: Int) {
+case class GameAsItem(replayRecordShared: ReplayRecordShared, ordering: Int) extends WithOrdering[GameAsItem]{
 
   @html
   val content = <div>
-    <span class="order-gi">{order.toString}</span>
-    <span class="time-gi">{replayRecordShared.dateGame.getOrElse("???")}</span>
-    <span class="nombre-gi">{replayRecordShared.nombreOriginal}</span>
+    <span class="order-gi font-weight-bold">Game {ordering.toString}</span>
+    <span class="time-gi">{replayRecordShared.dateGame.map(d => if(d.contains('T')) d.dropWhile(_ != 'T').tail else d ).getOrElse("???")}</span>
+    <span class="name-gi font-weight-bold">{replayRecordShared.nombreOriginal}</span>
   </div>
+
+  override def withNewOrder(order: Int): GameAsItem = copy(ordering = order)
 }
--- client/src/main/scala/ordergames/OrderGames.scala
+++ client/src/main/scala/ordergames/OrderGames.scala
@@ -2,6 +2,7 @@ package ordergames
 
 import com.thoughtworks.binding.Binding.Vars
 import org.lrng.binding.html
+import org.scalajs.dom.Event
 import shared.UtilParser
 import shared.models.ReplayRecordShared
 import upickle.default.read
@@ -9,16 +10,34 @@ import upickle.default.read
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 @JSExportTopLevel("OrderGames")
 class OrderGames(tournamentName: String, matchesString: String,  container: String){
-  private val matches = Vars[GameAsItem](read[Seq[ReplayRecordShared]](UtilParser.safeString2Json(matchesString)).zipWithIndex.map{case (g,i) => GameAsItem(g,i)}: _*)
+  private val matches = Vars[GameAsItem](read[Seq[ReplayRecordShared]](UtilParser.safeString2Json(matchesString)).zipWithIndex.map{case (g,i) => GameAsItem(g,i+1)}: _*)
+  val organizableMatches = new Organizable(matches)
 
   @html
   val module = {
-    <div>
-      {for(g <- matches) yield{
-          g.content
-        }
-      }
-    </div>
+    <ul class="list-group">
+      {for (g <- organizableMatches.sortedElements) yield {
+      <li class="list-group-item">
+        <div class="row-gi">
+          <div>
+            {g.content}
+          </div>
+          <button
+          type="button"
+          class="btn btn-primary"
+          onclick={_: Event => organizableMatches.assignLowerOrder(g)}>
+            UP
+          </button>
+          <button
+          type="button"
+          class="btn btn-primary"
+          onclick={_: Event => organizableMatches.assignHigherOrder(g)}>
+            DOWN
+          </button>
+        </div>
+      </li>
+      }}
+    </ul>
   }
 
   @JSExport("showPanel")
