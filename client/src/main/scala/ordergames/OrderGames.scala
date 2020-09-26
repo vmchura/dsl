480977ccfffdeb91b247c03af6b39fbd5065c14c
vmchura <vmchura@gmail.com>
2020-09-26 10:29:38 -0500

    removed some warnings

--- client/src/main/scala/ordergames/OrderGames.scala
+++ client/src/main/scala/ordergames/OrderGames.scala
@@ -1,22 +1,22 @@
 package ordergames
 
-import com.thoughtworks.binding.Binding
 import com.thoughtworks.binding.Binding.Vars
 import org.lrng.binding.html
-import org.scalajs.dom.raw.{HTMLButtonElement, HTMLFormElement, KeyboardEvent}
+import org.scalajs.dom.raw.{HTMLFormElement, KeyboardEvent}
 import org.scalajs.dom.{Event, document}
 import shared.UtilParser
 import shared.models.ReplayRecordShared
 import upickle.default.read
 
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+import scala.xml.NodeBuffer
 @JSExportTopLevel("OrderGames")
 class OrderGames(tournamentName: String, matchesString: String,  container: String){
   private val matches = Vars[GameAsItem](read[Seq[ReplayRecordShared]](UtilParser.safeString2Json(matchesString)).zipWithIndex.map{case (g,i) => GameAsItem(g,i)}: _*)
   val organizableMatches = new Organizable(matches)
   val bof = new BestOfBlock("bof")
   @html
-  val module = {
+  val module: NodeBuffer = {
 
     <ul class="list-group">
       {for (g <- organizableMatches.sortedElements) yield {
