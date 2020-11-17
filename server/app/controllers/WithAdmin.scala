4a7d6f364c38c02dc47be4e89a1333e1b5996f49
vmchura <vmchura@gmail.com>
2020-11-17 16:34:19 -0500

    Krakkatoa as Admin

--- server/app/controllers/WithAdmin.scala
+++ server/app/controllers/WithAdmin.scala
@@ -9,18 +9,21 @@ import scala.concurrent.Future
 
 case class WithAdmin() extends Authorization[User, CookieAuthenticator] {
 
-  def isAuthorized[B](user: User, authenticator: CookieAuthenticator)(
-    implicit request: Request[B]): Future[Boolean] = {
+  def isAuthorized[B](user: User, authenticator: CookieAuthenticator)(implicit
+      request: Request[B]
+  ): Future[Boolean] = {
 
     Future.successful(WithAdmin.isModeradorID(user.loginInfo.providerKey))
   }
 }
-object WithAdmin{
+object WithAdmin {
   private val moderadoresID = List(
-    "361607214156480512",  //kogler
+    "361607214156480512", //kogler
     "562728682712465415", //kishigel
     "662869207633100840", //araknoides
-    "698648718999814165"  //vmchq
+    "698648718999814165", //vmchq
+    "578353624699109435" //krakatoa
   )
-  def isModeradorID(discordID: String): Boolean = moderadoresID.exists(_.equals(discordID))
+  def isModeradorID(discordID: String): Boolean =
+    moderadoresID.exists(_.equals(discordID))
 }
