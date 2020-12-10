be34172997d9dae4255d921ebee8b2a89bef2873
vmchura <vmchura@gmail.com>
2020-12-10 15:47:47 -0500

    remove unused imports

--- server/test/models/services/DiscordFileServiceImplTest.scala
+++ server/test/models/services/DiscordFileServiceImplTest.scala
@@ -1,10 +1,7 @@
 package models.services
 
 import java.io.File
-import java.util.UUID
 
-import models.MatchNameReplay
-import org.scalatest.FunSuite
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 
@@ -12,13 +9,21 @@ import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class DiscordFileServiceImplTest extends  PlaySpec with GuiceOneAppPerSuite{
-  val service: DiscordFileService = app.injector.instanceOf(classOf[DiscordFileService])
+class DiscordFileServiceImplTest extends PlaySpec with GuiceOneAppPerSuite {
+  val service: DiscordFileService =
+    app.injector.instanceOf(classOf[DiscordFileService])
 
   "A Discord File Service" should {
     "add file" in {
-      val f = service.pushFileOnChannel("728442814832312375",new File("/home/vmchura/Games/starcraft-remastered/drive_c/users/vmchura/My Documents/StarCraft/Maps/Replays/BisuVsProts/Stork vs Bisu blue.rep"),"comment","fileName")
-      Await.result(f,20.seconds)
+      val f = service.pushFileOnChannel(
+        "728442814832312375",
+        new File(
+          "/home/vmchura/Games/starcraft-remastered/drive_c/users/vmchura/My Documents/StarCraft/Maps/Replays/BisuVsProts/Stork vs Bisu blue.rep"
+        ),
+        "comment",
+        "fileName"
+      )
+      Await.result(f, 20.seconds)
       f.map(j => assert(j))
       //service.push(new File("/home/vmchura/Games/starcraft-remastered/drive_c/users/vmchura/My Documents/StarCraft/Maps/Replays/BisuVsProts/Stork vs Bisu blue.rep"),MatchNameReplay(UUID.randomUUID(),"1 D","TX","VmChQ","Trebol"))
     }
