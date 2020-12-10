ee32ca46ad5aef8f1b45c86c28026e0af0914a81
vmchura <vmchura@gmail.com>
2020-12-10 15:47:20 -0500

    remove unused imports

--- server/test/jobs/DiscordNameUpdaterTest.scala
+++ server/test/jobs/DiscordNameUpdaterTest.scala
@@ -1,14 +1,8 @@
 package jobs
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
-import org.scalatest.{BeforeAndAfterAll, MustMatchers, WordSpec, WordSpecLike}
-import akka.actor.Actor
-import jobs.DiscordNameUpdater.Update
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import org.specs2.matcher.ShouldMatchers
 
 class DiscordNameUpdaterTest extends PlaySpec with GuiceOneAppPerSuite {
-
 
   "An Echo actor" must {
 
