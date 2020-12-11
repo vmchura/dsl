b07e462870aef700bee0eda2b3c018eb8ab76279
vmchura <vmchura@gmail.com>
2020-12-11 17:21:09 -0500

    Controller RaceSurvivor

--- server/app/controllers/racesurvivor/RaceSurvivorController.scala
+++ server/app/controllers/racesurvivor/RaceSurvivorController.scala
@@ -8,21 +8,18 @@ import controllers.{
 import javax.inject._
 import play.api.i18n.I18nSupport
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import akka.actor.typed._
 
 import scala.concurrent.duration._
 import akka.actor.typed.ActorRef
 import play.api.mvc.{Action, AnyContent}
-
-import akka.actor.typed.scaladsl.AskPattern._
 import akka.util.Timeout
-import modules.racesurvivor.CookieFabric._
-import modules.racesurvivor.CookieFabric
+import modules.gameparser.GameReplayManager.ManagerCommand
 @Singleton
 class RaceSurvivorController @Inject() (
     scc: SilhouetteControllerComponents,
-    val cookieFabric: ActorRef[CookieFabric.GiveMeCookies]
+    replayGameManager: ActorRef[ManagerCommand]
 )(implicit
     assets: AssetsFinder,
     ex: ExecutionContext,
@@ -32,12 +29,16 @@ class RaceSurvivorController @Inject() (
   implicit val timeout: Timeout = 5.seconds
   def sayHello(name: String): Action[AnyContent] = {
     Action.async { _ =>
+      Future.successful(Ok(s"Hola $name"))
+
+    }
+    /* Action.async { _ =>
       cookieFabric.ask(ref => CookieFabric.GiveMeCookies(6, ref)).map {
         case Cookies(cookies) => Ok(s"Hello $name, you've got $cookies cookies")
         case InvalidRequest(_) =>
           Ok(s"Hello $name, you've got nothing, good day sr")
       }
 
-    }
+    }*/
   }
 }
