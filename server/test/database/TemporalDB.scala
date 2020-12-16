e74844cdfbd1df9b84569b47f29e26886868e32c
vmchura <vmchura@gmail.com>
2020-12-16 16:43:53 -0500

    App per suite and added add smurf to user

--- server/test/database/TemporalDB.scala
+++ server/test/database/TemporalDB.scala
@@ -3,11 +3,11 @@ package database
 import com.google.inject.AbstractModule
 import com.mohiva.play.silhouette.api.Environment
 import com.mohiva.play.silhouette.test.FakeEnvironment
-import models.Tournament
-import models.services.TournamentService
+import models.{DiscordID, Smurf, Tournament, User}
+import models.services.{SmurfService, TournamentService}
 import net.codingwell.scalaguice.ScalaModule
-import org.scalatest.TestData
-import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import org.scalatest.{BeforeAndAfterEach, TestData}
+import org.scalatestplus.play.guice.{GuiceOneAppPerSuite, GuiceOneAppPerTest}
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Seconds, Span}
 import org.scalatestplus.play._
@@ -15,8 +15,14 @@ import play.api.Application
 import play.api.inject.guice.GuiceApplicationBuilder
 import utils.auth.DefaultEnv
 import DataBaseObjects._
+import models.services.SmurfService.SmurfAdditionResult.AdditionResult
+
 import scala.concurrent.ExecutionContext.Implicits.global
-trait TemporalDB extends PlaySpec with GuiceOneAppPerTest with ScalaFutures {
+trait TemporalDB
+    extends PlaySpec
+    with GuiceOneAppPerSuite
+    with ScalaFutures
+    with BeforeAndAfterEach {
   implicit val env: Environment[DefaultEnv] =
     new FakeEnvironment[DefaultEnv](Seq(first_user.loginInfo -> first_user))
   class FakeModule extends AbstractModule with ScalaModule {
@@ -24,20 +30,19 @@ trait TemporalDB extends PlaySpec with GuiceOneAppPerTest with ScalaFutures {
       bind[Environment[DefaultEnv]].toInstance(env)
     }
   }
+  System.setProperty("config.resource", "test-application.conf")
+
+  override lazy val app: Application = GuiceApplicationBuilder()
+    .configure(Map("ehcacheplugin" -> "disabled"))
+    .overrides(new FakeModule)
+    .build()
+
   implicit override val patienceConfig: PatienceConfig =
     PatienceConfig(timeout = Span(5, Seconds))
 
-  override def newAppForTest(td: TestData): Application = {
+  override def beforeEach(): Unit = {
     import sys.process._
-
-    "mongo dsl-test --eval \"db.dropDatabase();\"".!
-
-    System.setProperty("config.resource", "test-application.conf")
-
-    GuiceApplicationBuilder()
-      .configure(Map("ehcacheplugin" -> "disabled"))
-      .overrides(new FakeModule)
-      .build()
+    "mongo dsl-test --eval \"db.dropDatabase();\"".!(ProcessLogger(_ => ()))
 
   }
 
@@ -48,6 +53,14 @@ trait TemporalDB extends PlaySpec with GuiceOneAppPerTest with ScalaFutures {
       .saveTournament(
         Tournament(1L, "1Str", "DServer", "TorunamentName", active = true)
       )
+      .futureValue
+  }
+
+  protected def addSmurfToUser(user: User, smurf: Smurf): AdditionResult = {
+    val smurfService: SmurfService =
+      app.injector.instanceOf(classOf[SmurfService])
+    smurfService
+      .addSmurf(DiscordID(user.loginInfo.providerKey), smurf)
       .futureValue
   }
 
