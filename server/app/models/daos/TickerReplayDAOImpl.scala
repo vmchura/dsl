386cfe39c126c7f50cc94ddb448edc2d37bfd40d
vmchura <vmchura@gmail.com>
2020-12-17 01:41:51 -0500

    ticket with configuration

--- server/app/models/daos/TickerReplayDAOImpl.scala
+++ server/app/models/daos/TickerReplayDAOImpl.scala
@@ -1,16 +1,31 @@
 package models.daos
 import java.util.UUID
-
 import org.joda.time.DateTime
+import play.api.Configuration
 
-class TickerReplayDAOImpl extends TicketReplayDAO {
-  val upload = new TicketManager(10,1)
-  val download = new TicketManager(30,30)
-  override def ableToUpload(userID: UUID, now: DateTime): Boolean = upload.isAble(userID, now)
+import javax.inject.Inject
 
-  override def uploading(userID: UUID, now: DateTime): Unit = upload.marksAsUsing(userID, now)
+class TickerReplayDAOImpl @Inject() (configuration: Configuration)
+    extends TicketReplayDAO {
+  val upload =
+    new TicketManager(
+      configuration.get[Int]("user-ticket.time-window.upload"),
+      1
+    )
+  val download =
+    new TicketManager(
+      configuration.get[Int]("user-ticket.time-window.download"),
+      30
+    )
+  override def ableToUpload(userID: UUID, now: DateTime): Boolean =
+    upload.isAble(userID, now)
 
-  override def ableToDownload(userID: UUID, now: DateTime): Boolean = download.isAble(userID, now)
+  override def uploading(userID: UUID, now: DateTime): Unit =
+    upload.marksAsUsing(userID, now)
 
-  override def downloading(userID: UUID, now: DateTime): Unit = download.marksAsUsing(userID, now)
+  override def ableToDownload(userID: UUID, now: DateTime): Boolean =
+    download.isAble(userID, now)
+
+  override def downloading(userID: UUID, now: DateTime): Unit =
+    download.marksAsUsing(userID, now)
 }
