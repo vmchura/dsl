29b9e61130d413258021782d62486dc862ee4919
vmchura <vmchura@gmail.com>
2021-01-26 22:47:44 -0500

    Ignoring test case load first job

--- server/test/modules/teamsystem/LoadHistoryUserToLoggedTest.scala
+++ server/test/modules/teamsystem/LoadHistoryUserToLoggedTest.scala
@@ -2,12 +2,14 @@ package modules.teamsystem
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import models.daos.UserHistoryDAO
+import org.scalatest.Ignore
 import org.scalatest.wordspec.AnyWordSpecLike
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 
 import scala.concurrent.duration.DurationInt
 import scala.language.postfixOps
 
+@Ignore
 class LoadHistoryUserToLoggedTest
     extends ScalaTestWithActorTestKit
     with AnyWordSpecLike
@@ -16,15 +18,17 @@ class LoadHistoryUserToLoggedTest
   "Insert data" must {
     "Into logged" in {
       val userDAO = app.injector.instanceOf(classOf[UserHistoryDAO])
-      val ids = userDAO.all().futureValue.map(_.discordID).distinct.take(4)
+      val ids = userDAO.all().futureValue.map(_.discordID).distinct.drop(40)
       val workerBehavior = app.injector.instanceOf(classOf[DiscordPlayerWorker])
       val probe = testKit
         .createTestProbe[DiscordPlayerWorker.DiscordPlayerWorkerResponse]("")
       val supervisor = testKit.spawn(DiscordPlayerSupervisor(workerBehavior))
-      ids.foreach { id =>
-        println(id)
-        supervisor ! DiscordPlayerSupervisor.Register(id, Some(probe.ref))
-        probe.expectMessage(10 seconds, DiscordPlayerWorker.Registered())
+      ids.zipWithIndex.foreach {
+        case (id, indx) =>
+          Thread.sleep((3 seconds).toMillis)
+          supervisor ! DiscordPlayerSupervisor.Register(id, Some(probe.ref))
+          val messageResponse = probe.receiveMessage(10 seconds)
+          println(s"$indx] $messageResponse")
       }
     }
   }
