514df6cc08f31d974e8e6ca1521cac64dffc451d
vmchura <vmchura@gmail.com>
2021-03-03 11:11:50 -0500

    definition of variables to upload

--- client/src/main/scala/teamsystem/UploadTeamReplayComponent.scala
+++ client/src/main/scala/teamsystem/UploadTeamReplayComponent.scala
@@ -4,12 +4,15 @@ import com.thoughtworks.binding.Binding
 import com.thoughtworks.binding.Binding.Var
 import org.lrng.binding.html
 import org.scalajs.dom.raw.{File, HTMLTableRowElement}
+import shared.models.StarCraftModels.OneVsOne
 
 import scala.xml.Elem
 
 class UploadTeamReplayComponent(file: File) {
   private val rowClass = Var[String]("table-light")
   implicit def elem2TableRow(e: Elem): Binding[HTMLTableRowElement] = ???
+  private val messageError = Var[Option[String]](None)
+  private val oneVsOne = Var[Option[OneVsOne]](None)
 
   @html
   def render(): Binding[HTMLTableRowElement] = {
