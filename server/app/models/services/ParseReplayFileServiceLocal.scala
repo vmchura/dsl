3cc86f731dca1dc8cf55dc91307062ba4d6f4547
vmchura <vmchura@gmail.com>
2021-03-03 14:20:56 -0500

    ParseFileLocal

---
+++ server/app/models/services/ParseReplayFileServiceLocal.scala
@@ -0,0 +1,23 @@
+package models.services
+
+import java.io.File
+import scala.concurrent.Future
+
+class ParseReplayFileServiceLocal extends ParseReplayFileService {
+  def parseFile(file: File): Future[Either[String, String]] = {
+    import sys.process._
+    val buffer = new StringBuilder()
+    val path = "\"" + { file.getAbsolutePath } + "\""
+    val res =
+      s"/home/vmchura/Games/screp/cmd/screp/screp $path"
+        .!(ProcessLogger(line => buffer.append(line)))
+    if (res == 0) {
+      Future.successful(Right(buffer.toString()))
+    } else {
+      println(buffer.toString())
+      Future.failed(
+        new IllegalArgumentException("screp not installed correctly")
+      )
+    }
+  }
+}
--- server/app/modules/ParseReplayFileModuleEmpty.scala
+++ server/app/modules/ParseReplayFileModuleEmpty.scala
@@ -1,14 +1,17 @@
 package modules
 
 import com.google.inject.AbstractModule
-import models.services.{ParseReplayFileService, ParseReplayFileServiceEmptyImpl}
+import models.services.{
+  ParseReplayFileService,
+  ParseReplayFileServiceEmptyImpl,
+  ParseReplayFileServiceLocal
+}
 import net.codingwell.scalaguice.ScalaModule
 
 class ParseReplayFileModuleEmpty extends AbstractModule with ScalaModule {
 
   override def configure(): Unit = {
-    bind[ParseReplayFileService].to[ParseReplayFileServiceEmptyImpl]
+    bind[ParseReplayFileService].to[ParseReplayFileServiceLocal]
   }
-
 
 }
