fd5677db3e7be00559ea6cbbbabacebafbcd1e4c
vmchura <vmchura@gmail.com>
2021-03-18 20:22:11 -0500

    navbar working

--- tailwindcss-postcss/tailwind.config.js
+++ tailwindcss-postcss/tailwind.config.js
@@ -1,3 +1,4 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   purge: {
     content: ['../server/app/views/**/*.scala.html',
@@ -5,10 +6,21 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      primary: colors.violet,
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber
+    }
   },
   variants: {
     extend: {},
   },
   plugins: [],
+
 }
